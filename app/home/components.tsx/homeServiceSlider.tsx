5c3f996a9befe5860377bf6a833713fce20d0cf5
akmmubashir <akmmubashir@gmail.com>
2025-11-26 23:24:10 +0530

    changes

--- app/home/components.tsx/homeServiceSlider.tsx
+++ app/home/components.tsx/homeServiceSlider.tsx
@@ -30,31 +30,37 @@ const HomeServiceSlider = ({
     dots: false,
     infinite: true,
     speed: 700,
-    mobileFirst: true,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: autoplayIntervalMs,
     arrows: false,
     responsive: [
       {
-        breakpoint: 640, // >=640px
+        breakpoint: 5000,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 1024, // >=1024px
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 1280, // >=1280px
+        breakpoint: 768,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
