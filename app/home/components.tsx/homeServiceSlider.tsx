d3348a8bfc580d2379e6b11f8ef50f694161524e
akmmubashir <akmmubashir@gmail.com>
2025-11-26 23:35:04 +0530

    changes

--- app/home/components.tsx/homeServiceSlider.tsx
+++ app/home/components.tsx/homeServiceSlider.tsx
@@ -30,7 +30,7 @@ const HomeServiceSlider = ({
     dots: false,
     infinite: true,
     speed: 700,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: autoplayIntervalMs,
