f7b02d30308f749267bf45de6b240c305267ce08
akmmubashir <akmmubashir@gmail.com>
2025-11-26 20:56:11 +0530

    image changes

--- app/home/components.tsx/ourServices.tsx
+++ app/home/components.tsx/ourServices.tsx
@@ -9,39 +9,39 @@ const OurServices = () => {
       id: 1,
       title: "Space Frame Structures",
       description: "Lightweight, high-strength architectural systems",
-      img: "/assets/services/space-frame-structures.jpg",
+      img: "/assets/services/space-frame-structures.webp",
     },
     {
       id: 2,
       title: "PEB (Pre-Engineered Buildings)",
       description: "Fast, efficient, and durable steel buildings",
-      img: "/assets/services/pre-engineered-buildings.jpg",
+      img: "/assets/services/pre-engineered-buildings.webp",
     },
     {
       id: 3,
       title: "Tensile Roofing Architecture",
       description: "Aesthetic, modern, and weather-resistant structures",
-      img: "/assets/services/tensile-roofing-architecture.jpg",
+      img: "/assets/services/tensile-roofing-architecture.webp",
     },
     {
       id: 4,
       title: "Sandwich Panel Roofing",
       description: "Energy-efficient and insulated roofing systems",
-      img: "/assets/services/sandwich-panel-roofing.jpg",
+      img: "/assets/services/sandwich-panel-roofing.webp",
     },
     {
       id: 5,
       title: "Steel Structural Fabrication",
       description:
         "Precision-built components manufactured to the highest standards",
-      img: "/assets/services/steel-structural-fabrication.jpg",
+      img: "/assets/services/steel-structural-fabrication.webp",
     },
     {
       id: 6,
       title: "Commercial & Industrial Roofing",
       description:
         "Long-lasting solutions for factories, malls, warehouses, and more",
-      img: "/assets/services/commercial-industrial-roofing.jpg",
+      img: "/assets/services/commercial-industrial-roofing.webp",
     },
   ];
   return (
@@ -59,14 +59,14 @@ const OurServices = () => {
       </div>
       <div className="col-span-5 max-lg:col-span-full -rotate-y-180">
         <Image
-          src={"/assets/services/service.jpg"}
+          src={"/assets/services/service.webp"}
           alt="service"
           width={400}
           height={250}
           className="object-cover w-full rounded-[20px_60px] max-lg:hidden"
         />
         <Image
-          src={"/assets/services/serviceMob.jpg"}
+          src={"/assets/services/serviceMob.webp"}
           alt="service"
           width={400}
           height={250}
